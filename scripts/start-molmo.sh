c1cd45c75193a8b2663169f2830f55ba8e5483f0
Logan Keenan <d.logankeenan@gmail.com>
2024-10-19 12:47:49 -0500

    add script for starting molmo server with vllm

---
+++ scripts/start-molmo.sh
@@ -0,0 +1,27 @@
+docker run --runtime=nvidia --gpus all \
+  -v ~/.cache/huggingface:/root/.cache/huggingface \
+  -p 8000:8000 \
+  --ipc=host \
+  vllm/vllm-openai:latest \
+  --model allenai/Molmo-7B-D-0924 \
+  --trust-remote-code
+
+
+#base64 -w 0 /home/logan/Pictures/Screenshots/test.png > image_base64.txt
+#
+#
+#curl --location 'http://localhost:8000/v1/chat/completions' \
+#--header 'Content-Type: application/json' \
+#--header 'Authorization: Bearer token' \
+#--data '{
+#     "model": "allenai/Molmo-7B-D-0924",
+#    "messages": [
+#      {
+#        "role": "user",
+#        "content": [
+#            {"type" : "text", "text": "Find the sign up button and return the coordinates. The response should only be in the follow format: [x, y]"},
+#            {"type": "image_url", "image_url": {"url": "data:image/jpeg;base64,'"$(cat image_base64.txt)"'"}}
+#        ]
+#      }
+#    ]
+#  }'
